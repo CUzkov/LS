6d014e1c1b31906a180fa757411d59dab1434e65
CUzkov <UskovDanek@gmail.com>
2021-10-25 17:01:35 +0300

    Init projects

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+rust-lang - 1.54
+
+run project:
+sudo docker build -t server .
+sudo docker container run --network host server
---
+++ frontend/site/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { FC } from 'react';
+
+import './style.scss';
+
+export const App: FC = () => {
+    return (
+        <div className="wrapper">
+            <h1>React 17 and TypeScript 4 App!🚀</h1>
+        </div>
+    );
+};
