9da5ef851448218a1ce4f29cbe806165076ba8db
CUzkov <UskovDanek@gmail.com>
2021-10-25 19:08:46 +0300

    Adding redis

--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,8 +1,27 @@
 #[macro_use] extern crate rocket;
 
+extern crate redis;
+use redis::Commands;
+
+fn get_param() -> Result<String, redis::RedisError> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42)?;
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    let result: String = con.get("my_key")?;
+
+    Result::Ok(result)
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    let result: String = get_param().unwrap();
+
+    result
 }
 
 #[launch]
